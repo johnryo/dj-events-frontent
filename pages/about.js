d8c7f348bd9736d1ca791ab4f831249749fc1359
John Oggerino <jpogge55@gmail.com>
2021-04-20 13:10:08 -0700

    Initial commit: pages and routing

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+export default function AboutPage() {
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <p>This is an app to find DJ and other musical events.</p>
+      <p>Version 1.0.0</p>
+
+      <style jsx>{`
+        .about {
+          color: #2f4f4f;
+        }
+      `}</style>
+    </div>
+  )
+}
