bfc6e5ae228313b245e32bd5ee34941178ad8629
John Oggerino <jpogge55@gmail.com>
2021-04-22 00:04:40 -0700

    Layout, Header and Footer

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,11 @@
+import Layout from '../components/Layout'
+
 export default function AboutPage() {
   return (
-    <div className="about">
+    <Layout title='About DJ Events'>
       <h1>About</h1>
       <p>This is an app to find DJ and other musical events.</p>
       <p>Version 1.0.0</p>
-
-      <style jsx>{`
-        .about {
-          color: #2f4f4f;
-        }
-      `}</style>
-    </div>
+    </Layout>
   )
 }
